48ec2808678df4e7827571d87e450ae79f90fbce
soniagkalliath <soniageorge111@gmail.com>
2024-11-08 12:43:50 +0530

    first commit

---
+++ database/dbConnection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const connectionString = process.env.DBCONNECTIONSTRING
+
+mongoose.connect(connectionString).then(res=>{
+    console.log("MongoDB Atlas connected succesfully with pfServer");
+}).catch(err=>{
+    console.log("Mongodb Atlas connection failed");
+    console.log(err);    
+})
